e9db96fd904d90ac9696191a902d555db96ad499
yoyoking1774 <51570330+yoyoking1774@users.noreply.github.com>
2020-11-17 15:06:07 +0800

    Update AnimationIN.cs

--- MoneyisLand_Test/Assets/02_Script/AnimationIN.cs
+++ MoneyisLand_Test/Assets/02_Script/AnimationIN.cs
@@ -1,22 +1,22 @@
-﻿//全部動畫控制
+﻿////全部動畫控制
 using UnityEngine;
 using System.Collections;
 
 public class AnimationIN : MonoBehaviour {
-	//宣告變數===================================================================
-	//Work頁面進入動畫是否播放 true:播放 fasle:未播放
+	////宣告變數===================================================================
+	////Work頁面進入動畫是否播放 true:播放 fasle:未播放
 	public static bool WorkPage_IN = false;
-	//宣告動畫===================================================================
-	//WorkPage進入動畫
+	////宣告動畫===================================================================
+	////WorkPage進入動畫
 	public Animation Work_IN;
 
 
-	// Use this for initialization
+	//// Use this for initialization
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
+	//// Update is called once per frame
 	void Update () {
 		//播放WorkPage進入動畫
 		if (WorkPage_IN) {
